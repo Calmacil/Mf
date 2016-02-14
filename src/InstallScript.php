922da2f1211eae4f7b2f89eecf396e9de6c7cbe8
Calmacil <togo.harrian@gmail.com>
2016-02-14 23:54:44 +0100

    Fixed the install script; This is the final release.

--- src/InstallScript.php
+++ src/InstallScript.php
@@ -27,5 +27,19 @@ class InstallScript
             if (!file_exists($dir))
                 mkdir($dir, 0755);
         }
+
+        $files = array(
+            __DIR__ . "/../resources/settings_dev.json" => "config/settings_dev.json",
+            __DIR__ . "/../resources/db.json"           => "config/db.json",
+            __DIR__ . "/../resources/routing.json"      => "config/routing.json",
+            __DIR__ . "/../resources/index.php"         => "web/index_dev.php"
+        );
+
+        foreach ($files as $orig => $dest) {
+            if (!file_exists($dest)) {
+                copy($orig, $dest);
+                chmod($dest, 0644);
+            }
+        }
     }
 }
