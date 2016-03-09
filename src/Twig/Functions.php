3f46eaf07c43efd19b8e39372bee4a74352a1abc
Calmacil <togo.harrian@gmail.com>
2016-03-10 00:16:14 +0100

    Debug

--- src/Twig/Functions.php
+++ src/Twig/Functions.php
@@ -14,16 +14,13 @@ use Calma\Mf\Routing\Router;
 class Functions
 {
     /**
-     * @param $route_name
-     * @param null $params
+     * @param \Twig_Environment $env
+     * @param mixed $route_name
+     * @param mixed $params
      * @return string
      */
-    public static function url($env, $route_name, $params=null)
+    public static function url(\Twig_Environment $env, $route_name, $params=null)
     {
-        var_dump($env);
-        var_dump($route_name);
-        var_dump($params);
-        return true;
         return Router::getInstance()->generateRoute($route_name, $params);
     }
 }
